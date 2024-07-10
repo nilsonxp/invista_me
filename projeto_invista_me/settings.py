e0c4af1b0b91822045a5317ff19e6e5997d40be4
nilsonxp <nilsonxp@gmail.com>
2024-07-10 11:25:48 -0400

    prep para render

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
+# import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-d3wr%@!=b42m@8c^_arcwgx2@prl8yvlxpl4sym8e#-bc$(mxe"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "projeto_invista_me.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres.lwbvjeumcxousprisnyr', 
+        'PASSWORD': '3YuFsUI2NYPRBe8H',
+        'HOST': 'aws-0-sa-east-1.pooler.supabase.com', 
+        'PORT': '6543'
     }
 }
 
